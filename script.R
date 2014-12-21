4b790156e195cff17f35d2293e2a630cc2d8205d
holybasil <benjaminsw@live.com>
2014-12-21 15:08:00 +0700

    add comments and formatted code

--- script.R
+++ script.R
@@ -6,6 +6,9 @@
    ###The min, mean, mode and max of moves used for the level.
 ###A text describing how the above data differs for the different versions of the game.
 
+
+
+#read data file into R
 data <- read.csv('cleaned_data.csv', header = TRUE)
 data$Event <- as.character(data$Event)
 
@@ -15,31 +18,6 @@ passfail <- as.data.frame(table(data$Event))
 
 ###The min, mean, mode and max of retries (how many failures did a user have on a particular level before succeding
 #device. installdate, devicemodel, facebookid
-
-####-------------------------------------------------------------------
-#data$failed <- data$LevelNumber == data$FurthestLevel
-#faileddata <- subset(data, data$failed == TRUE, )
-#min <- nrow(faileddata); max <- 0; sum <- 0; mode <- 0; n <- 0
-
-#for (i in unique(faileddata$Session)){
-  #tmp <- subset(faileddata, faileddata$Session == i,)
-  #for (j in unique(tmp$LevelNumber)){
-    #failures <- subset(tmp, tmp$LevelNumber == j,)
-    #if (nrow(failures) < min) min <- nrow(failures)
-    #if (nrow(failures) > max) max <- nrow(failures)
-    #if (is.na(mode[nrow(failures)])) mode[nrow(failures)] <- 0
-    #mode[nrow(failures)] = mode[nrow(failures)] + 1
-    #n = n+1
-  #}
-#}
-#min
-#max
-#mode
-#nrow(faileddata)/n
-#n
-###--------------------------------------------------------------------------------------
-
-
 data$pseudoID <- paste(data$Device, data$InstallDate, data$DeviceModel, data$FacebookID, sep = '')
 retries <- subset(data, data$Event == '002_RetryLevel', )
 retstat <- data.frame(Level = numeric(), Min = numeric(), Max = numeric(),  Mode = character(), Mean = numeric(), stringsAsFactors=FALSE)                  
@@ -66,26 +44,7 @@ for(i in unique(sort(retries$LevelNumber))){
 
 }
 #write.csv(retstat, file = 'retstat.csv')
-#device. installdate, devicemodel, facebookid
-faileddata$pseudoID <- paste(faileddata$Device, faileddata$InstallDate, faileddata$DeviceModel, faileddata$FacebookID, sep = '')
-min <- nrow(faileddata); max <- 0; sum <- 0; mode <- 0; n <- 0
 
-for (i in unique(faileddata$pseudoID)){
-  tmp <- subset(faileddata, faileddata$pseudoID == i,)
-  for (j in unique(tmp$LevelNumber)){
-    failures <- subset(tmp, tmp$LevelNumber == j,)
-    if (nrow(tmp) != 0 & nrow(failures) < min) min <- nrow(failures)
-    if (nrow(tmp) != 0 & nrow(failures) > max) max <- nrow(failures)
-    if (is.na(mode[nrow(failures)])) mode[nrow(failures)] <- 0
-    mode[nrow(failures)] = mode[nrow(failures)] + 1
-    n = n+1
-  }
-}
-min
-max
-mode
-nrow(faileddata)/n
-n
 
 ###The min, mean, mode and max of moves used for the level.
 library(modeest)
@@ -101,17 +60,26 @@ movesused <- cbind(tmp1df, tmp2df)
 #lapply(tmp2, write, "tmp2.csv", append = TRUE, ncolumns=1000, sep = ",")
 
 ###A text describing how the above data differs for the different versions of the game.
+#passes/failures number for each version
 pfversion <- as.data.frame(table(data$Version, data$Event))
 colnames(pfversion) <- c("Version", "Event", "Count")
 pfversion <- subset(pfversion, pfversion$Event == "101_LevelSuccess" | pfversion$Event == "102_LevelFailed",)
-rtsversion <- tapply(faileddata$LevelNumber, faileddata$Version, summary)
-rtsversiondf <- as.data.frame(do.call(rbind, rtsversion))
-rtsversiondf <- rtsversiondf[,c(1,4,6)]
+#retries number for each version
+rtsv <- tapply(faileddata$LevelNumber, faileddata$Version, summary)
+rtsvdf <- as.data.frame(do.call(rbind, rtsv))
+rtsvdf <- rtsvdf[,c(1,4,6)]
+rtsv2 <- tapply(faileddata$LevelNumber, faileddata$Version, mfv, na.rm = TRUE)
+rtsv2df <- as.data.frame(do.call(rbind, rtsv2))
+colnames(rtsv2df)[1] <- "Mode"
+rtsv2df <- rtsv2df[,1:2]
+retriesversion<- cbind(rtsvdf, rtsv2df)
+#moved used for each version
 muv <- tapply(data$MovesUsed, data$Version, summary)
 muvdf <- as.data.frame(do.call(rbind, muv))
 muvdf <- muvdf[,c(1,4,6)]
-muv2 <- tapply(data$MovesUsed, data$Version, mlv, na.rm = TRUE)
+muv2 <- tapply(data$MovesUsed, data$Version, mfv, na.rm = TRUE)
 muv2df <- as.data.frame(do.call(rbind, muv2))
 muv2df <- muv2df[,1:2]
 colnames(muv2df)[1] <- "Mode"
-muversion <- cbind(muvdf, muv2df)
+muversion <- cbind(muvdf, muv2df$Mode)
+
